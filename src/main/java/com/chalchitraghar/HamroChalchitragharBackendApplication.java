d98e8fd43a9421acc479ff8db680d231e12edb0c
yuneshbyte01 <yuneshtimsina@gmail.com>
2026-01-09 15:10:13 +0545

    first commit

---
+++ src/main/java/com/chalchitraghar/HamroChalchitragharBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chalchitraghar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamroChalchitragharBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamroChalchitragharBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chalchitraghar/HamroChalchitragharBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalchitraghar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamroChalchitragharBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
